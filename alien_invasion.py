c8c69ca3c0ab7de7e3d5d89271fe9143769e04e6
Mervyn S. Philip <137993918+spmervyn7@users.noreply.github.com>
2026-07-30 17:52:11 -0400

    (P3) setup button to start the game and make the mouse disappear

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from arsenal import Arsenal
 from alien_fleet import AlienFleet
 from game_stats import GameStats
 from time import sleep
+from button import Button
 
 class AlienInvasion:
     
@@ -35,7 +36,9 @@ class AlienInvasion:
         self.ship = Ship(self, Arsenal(self))
         self.alien_fleet = AlienFleet(self)
         self.alien_fleet.create_Fleet()
-        self.game_active = True
+
+        self.play_button = Button(self, 'Play')
+        self.game_active = False
 
     def run_game(self) -> None:
         # Game loop
@@ -81,11 +84,25 @@ class AlienInvasion:
         self.alien_fleet.fleet.empty()
         self.alien_fleet.create_Fleet()
 
+    def restart_game(self):
+        # setting up dynamic settings
+        # reset game stats
+        # update HUD scores
+        self._reset_level()
+        self.ship._center_ship()
+        self.game_active = True
+        pygame.mouse.set_visible(False)
+
     def _update_screen(self) -> None:
         self.screen.blit(self.bg, (0, 0))
         self.ship.draw()
         self.ship.arsenal.draw()  # <-- ADDED: Draws laser projectiles onto the screen!
         self.alien_fleet.draw()
+
+        if not self.game_active:
+            self.play_button.draw()
+            pygame.mouse.set_visible(True)
+
         pygame.display.flip()
     
     def _check_events(self) -> None:
@@ -98,6 +115,13 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self._check_button_clicked()
+
+    def _check_button_clicked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.play_button.check_clicked(mouse_pos):
+            self.restart_game()
 
     def _check_keyup_events(self, event) -> None:
         if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
